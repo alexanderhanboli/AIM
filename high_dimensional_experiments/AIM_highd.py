c29bd584ec5dd1cfac2ef9fbe00e1cf7077b55f5
AlexanderYogurt <alexanderhanboli@gmail.com>
2018-12-07 22:38:18 -0800

    cahnge to kl

--- high_dimensional_experiments/AIM_highd.py
+++ high_dimensional_experiments/AIM_highd.py
@@ -9,18 +9,18 @@ class GeneratorX(nn.Module):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(zd, 16*zd),
-            #nn.BatchNorm1d(zd*16),
-            #nn.LeakyReLU(0.02),
+            nn.BatchNorm1d(zd*16),
+            nn.LeakyReLU(0.02),
 
             #nn.Dropout(0.2),
             nn.Linear(16*zd, 16*zd),
-            #nn.BatchNorm1d(zd*16),
-            #nn.LeakyReLU(0.02),
+            nn.BatchNorm1d(zd*16),
+            nn.LeakyReLU(0.02),
 
             #nn.Dropout(0.2),
             nn.Linear(16*zd, 16*zd),
-            #nn.BatchNorm1d(zd*16),
-            #nn.LeakyReLU(0.02),
+            nn.BatchNorm1d(zd*16),
+            nn.LeakyReLU(0.02),
 
             #nn.Dropout(0.2),
             nn.Linear(16*zd, 16*zd),
@@ -31,26 +31,27 @@ class GeneratorX(nn.Module):
         )
 
     def forward(self, x):
-        return F.tanh(self.net(x))
+        return self.net(x)
 
 class GeneratorZ(nn.Module):
     def __init__(self, zd=16, xd=256):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(xd, zd*8),
-            #nn.BatchNorm1d(zd*8),
-            #nn.LeakyReLU(0.02),
+            nn.BatchNorm1d(zd*8),
+            nn.LeakyReLU(0.02),
 
-            nn.Dropout(0.2),
+            #nn.Dropout(0.2),
             nn.Linear(zd*8, 4*zd),
-            #nn.BatchNorm1d(zd*4),
-            #nn.LeakyReLU(0.02),
+            nn.BatchNorm1d(zd*4),
+            nn.LeakyReLU(0.02),
 
-            nn.Dropout(0.2),
+            #nn.Dropout(0.2),
             nn.Linear(4*zd, zd*4),
             #nn.BatchNorm1d(zd*4),
             #nn.LeakyReLU(0.02),
 
+            nn.Linear(4*zd, zd*4),
             nn.Linear(zd*4, zd*2)
         )
 
@@ -63,14 +64,14 @@ class DiscriminatorX(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(xd, xd //4),
             nn.LeakyReLU(0.02),
-            nn.Dropout(0.5),
+            nn.Dropout(0.1),
 
             # nn.Linear(xd//4, xd//4),
             # nn.LeakyReLU(0.02),
 
             nn.Linear(xd//4, xd //8),
             nn.LeakyReLU(0.02),
-            nn.Dropout(0.5),
+            #nn.Dropout(0.1),
 
             nn.Linear(xd//8, 1)
         )
---
+++ high_dimensional_experiments/train_VAE.py
@@ -0,0 +1,203 @@
+from __future__ import (division, print_function, )
+from collections import OrderedDict
+from scipy.stats import multivariate_normal
+import numpy.random as npr
+import numpy as np
+from itertools import *
+
+from fuel import config
+from fuel.datasets import H5PYDataset, IndexableDataset
+from fuel.transformers.defaults import uint8_pixels_to_floatX
+from fuel.utils import find_in_data_path
+from fuel.streams import DataStream
+from fuel.schemes import ShuffledScheme
+
+import torch
+import torch.autograd as autograd
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+
+import scipy.misc
+import imageio
+import matplotlib.gridspec as gridspec
+import os, time, pickle
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import Gaussian_Sample_HighD as GS
+
+from scipy.stats import multivariate_normal, entropy
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-g', '--gpu', type=str, default='-1', metavar='GPU',
+                    help='set GPU id (default: -1)')
+parser.add_argument('-b', '--batch-size', type=int, default=32, metavar='N',
+                    help='input batch size for training (default: 32)')
+parser.add_argument('-e', '--epochs', type=int, default=100, metavar='E',
+                    help='how many epochs to train (default: 100)')
+parser.add_argument('--lr-g', type=float, default=1e-5, metavar='LR',
+                    help='initial ADAM learning rate of G (default: 1e-5)')
+parser.add_argument('--lr-d', type=float, default=1e-5, metavar='LR',
+                    help='initial ADAM learning rate of D (default: 1e-5)')
+parser.add_argument('--decay', type=float, default=0, metavar='D',
+                    help='weight decay or L2 penalty (default: 0)')
+parser.add_argument('-z', '--zdim', type=int, default=16, metavar='Z',
+                    help='dimension of latent vector (default: 16)')
+
+opt = parser.parse_args()
+
+import os
+import sys
+import numpy as np
+cuda = 0 if opt.gpu == -1 else 1
+if cuda:
+    os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu
+BS = opt.batch_size
+Zdim = opt.zdim
+IMAGE_PATH = 'VAE_images'
+MODEL_PATH = 'VAE_models'
+
+# ===============
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from itertools import chain
+from torchvision.utils import save_image
+from VAE_highd import *
+
+if not os.path.exists(IMAGE_PATH):
+    print('mkdir ', IMAGE_PATH)
+    os.mkdir(IMAGE_PATH)
+if not os.path.exists(MODEL_PATH):
+    print('mkdir ', MODEL_PATH)
+    os.mkdir(MODEL_PATH)
+
+def prog_print(e,b,b_total,loss_g,loss_e):
+    sys.stdout.write("\r%3d: [%5d / %5d] G: %.4f E: %.4f" % (e,b,b_total,loss_g,loss_e))
+    sys.stdout.flush()
+
+def train():
+    # load models
+    Gx = GeneratorX()
+    Gz = GeneratorZ()
+
+    # load dataset
+    # ==========================
+    train_data, valid_data, trans_mtx = GS.main()
+    train_dataset = GS.Gaussian_Data(train_data)
+    valid_dataset = GS.Gaussian_Data(valid_data)
+
+    dataloader = DataLoader(dataset=train_dataset,
+                            batch_size=BS,
+                            pin_memory= True,
+                            shuffle=True)
+    validloader = DataLoader(dataset=valid_dataset,
+                            batch_size=5000,
+                            pin_memory=True,
+                            shuffle=True)
+
+    N = len(dataloader)
+    # print(N)
+
+    z = torch.FloatTensor(BS, Zdim).normal_(0, 1)
+    z_pred = torch.FloatTensor(5000, Zdim).normal_(0, 1)
+    z_pred = Variable(z_pred)
+    noise = torch.FloatTensor(BS, Zdim).normal_(0, 1)
+
+    if cuda:
+        Gx.cuda()
+        Gz.cuda()
+        z, z_pred, noise = z.cuda(), z_pred.cuda(), noise.cuda()
+
+
+    # optimizer
+    optim_g = optim.Adam(chain(Gx.parameters(),Gz.parameters()),
+                         lr=opt.lr_g, betas=(.5, .999), weight_decay=opt.decay)
+
+    # train
+    # ==========================
+    softplus = nn.Softplus()
+    mse = nn.MSELoss()
+    for epoch in range(opt.epochs):
+        for i, (imgs, _) in enumerate(dataloader):
+            batch_size = imgs.size(0)
+            if cuda:
+                imgs = imgs.cuda()
+            imgs = Variable(imgs)
+            z.resize_(batch_size, Zdim).normal_(0, 1)
+            noise.resize_(batch_size, Zdim).normal_(0, 1)
+            zv = Variable(z)
+            noisev = Variable(noise)
+
+            # forward
+            encoded = Gz(imgs)
+            z_mu, z_sigma = encoded[:, :Zdim], encoded[:, Zdim:]
+            z_enc = z_mu + z_sigma.exp() * noisev
+            # So encoded[:, Zdim] is log(sigma)
+            imgs_fake = Gx(z_enc)
+
+            # compute loss
+            loss_g = mse(imgs_fake, imgs)
+            loss_e = -0.5 * torch.sum(1 + z_sigma - z_mu.pow(2) - z_sigma.exp())
+            loss_ge = loss_g + loss_e
+
+            # backward & update params
+            Gx.zero_grad()
+            Gz.zero_grad()
+            loss_ge.backward()
+            optim_g.step()
+
+            prog_print(epoch+1, i+1, N, loss_g.data[0], loss_e.data[0])
+
+        # generate fake images
+        # save_image(Gx(z_pred).data,
+        #            os.path.join(IMAGE_PATH,'%d.png' % (epoch+1)),
+        #            nrow=9, padding=1,
+        #            normalize=False)
+        # save models
+        print("-------> Saving models...")
+        torch.save(Gx.state_dict(),
+                   os.path.join(MODEL_PATH, 'Gx-%d.pth' % (epoch+1)))
+        torch.save(Gz.state_dict(),
+                   os.path.join(MODEL_PATH, 'Gz-%d.pth' % (epoch+1)))
+
+        # evaluate models
+        x_eval = Gx(z_pred)
+        x_eval = x_eval.data.cpu().numpy()
+        for i, (imgs, _) in enumerate(validloader):
+            if cuda:
+                imgs = imgs.cuda()
+            imgs = Variable(imgs)
+            z_eval = Gz(imgs)
+            break
+        # print(len(z_eval.data))
+
+        noise = Variable(torch.FloatTensor(5000, Zdim).normal_(0, 1).cuda())
+        z_sample = z_eval[:, :Zdim] + z_eval[:, Zdim:].exp() * noise
+        pk = multivariate_normal.pdf(z_sample.data, mean=np.zeros(16))
+        true_normal = np.random.randn(5000, Zdim)
+        qk = multivariate_normal.pdf(true_normal, mean=np.zeros(16))
+        print("The z entropy is {}".format(entropy(pk, qk)))
+
+        x_mean = np.dot(z_pred.data.cpu().numpy(), trans_mtx)
+        diff = np.subtract(x_eval, x_mean) ** 2
+
+        l2 = np.mean(np.sqrt(np.sum(diff, axis=1)))
+        print("The x reconstruction is {}\n".format(l2))
+
+        # z_logli = -torch.mean(torch.mean(0.5 * z_eval ** 2 + 0.5 + 0.5 * np.log(2*np.pi), 1))
+        # print("log-likehood of z is {}".format(z_logli.data))
+
+        # x_logli = -torch.mean(torch.mean(0.5 * (x_eval - MEAN) ** 2 + 0.5 + 0.5 * np.log(2*np.pi), 1))
+        # print("log-likehood of x is {}".format(x_logli.data))
+
+if __name__ == '__main__':
+    train()
